5c95346e8fc0fb48f0954bd20a0cc65e9dd3f6d0
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-11-03 21:32:28 +0500

    Qolganlari ham o'chdi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Address;
-use App\Models\Category;
-use App\Models\Course;
-use App\Models\Faculty;
-use App\Models\Field;
-use App\Models\Group;
-use App\Models\Masalliq;
-use App\Models\Ovqat;
-use App\Models\OvqatMasalliq;
-use App\Models\Post;
-use App\Models\Student;
-use App\Models\University;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -34,49 +22,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test1@example.com',
             'password' => '123456'
         ]);
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Ovqat::create(['name'=> 'Ovqat '.$i]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Masalliq::create(['name'=> 'Masalliq '.$i]);
-        // }
-
-        // for ($i=1; $i < 100; $i++) { 
-        //     OvqatMasalliq::create([
-        //         'ovqat_id'=> rand(1,10),
-        //         'masalliq_id'=> rand(1,10)
-        //     ]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     University::create(['name'=> 'University'.$i]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Faculty::create(['university_id' => rand(1,10),'name'=> 'Faculty'.$i]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Field::create(['faculty_id'=>rand(1,10),'name'=> 'Field'.$i]);
-        // }
-
-        // for ($i=1; $i < 7; $i++) { 
-        //     Course::create(['name'=> 'Course'.$i]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Group::create(['field_id'=>rand(1,10),'course_id'=>rand(1,6),'name'=> 'Group'.$i]);
-        // }
-
-        // for ($i=1; $i < 11; $i++) { 
-        //     Address::create(['name'=> 'Address'.$i]);
-        // }
-
-        // for ($i=1; $i < 2000; $i++) { 
-        //     Student::create(['group_id'=>rand(1,10),'address_id' => rand(1,10),'name'=> 'student'.$i,'phone'=>fake()->phoneNumber(),'image'=>fake()->address()]);
-        // }
 
         // for ($i=1; $i < 20; $i++) { 
         //     Category::create(['name'=>'Category'.$i]);
