53c9b7ada17cf251f22ad012b847acd0fd36bc33
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-11-03 21:29:14 +0500

    Keraksiz fayllar o'chirildi

--- routes/web.php
+++ routes/web.php
@@ -2,29 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\MasalliqController;
-use App\Http\Controllers\OvqatController;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\UniversityController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/',[OvqatController::class,'index'])->name('ovqat.index');
-// Route::post('/ovqat-create',[OvqatController::class,'store'])->name('ovqat.store');
-// Route::put('/ovqat-update/{ovqat}',[OvqatController::class,'update'])->name('ovqat.put');
-
-
-// Route::get('/masalliq',[MasalliqController::class,'index']);
-
-
-// Route::get('/',[UniversityController::class,'index'])->name('university');
-// Route::post('/university-create',[UniversityController::class,'store'])->name('university.create');
-// Route::put('/university-update/{university}',[UniversityController::class,'update'])->name('university.update');
-// Route::delete('/university-delete/{university}',[UniversityController::class,'destroy'])->name('university.delete');
 
 Route::get('/category',[CategoryController::class,'index'])->name('category.index');
 Route::post('/category-create',[CategoryController::class,'store'])->name('category.create');
